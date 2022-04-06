a3dcd4cfcf9a242e21c3d1c35f4064dd8d69d5c3
JetWai97 <74056345+JetWai97@users.noreply.github.com>
2022-04-07 02:28:28 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cwb46pkzcqhg.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanjetwai-bucket"
+customregion = "us-east-1"
